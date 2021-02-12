749e07f7616737f54024e2591d9b8b25c76f897d
henrysteve45 <henry.steve45@gmail.com>
2021-02-12 11:20:19 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    WMATA_KEY : '',     //Enter WMATA API key from https://developer.wmata.com/developer/
+    MAPBOX_KEY : '',    //Enter Mapbox API key from https://account.mapbox.com/
+    FAVIDS : [''],      //Enter favorite bus stop ID numbers (ex. '1000238', '1001122', '1000420')
+    FAVNAMES : ['']     //Enter favorite bus stop names (ex. 'Alabama Ave + Jasper St', '8th St + H St', 'Good Hope Road + 14th St')
+};
